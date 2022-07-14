7c8243d4e630c059629ab5d056b1d6d89e84f244
Rashmi Dayanand <dayanandrashmi2@gmail.com>
2022-07-14 11:32:37 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,15 +9,14 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t nelem;
+	int i = 0;
 
-	nelem = 0;
 	while (h != NULL)
 	{
-		if (h->str == NULL)
-			printf("[%d] %s\n", h->len, h->str);
+		printf("[0] (nil)\n");
 		h = h->next;
-		nelem++;
 	}
-	return (nelem);
+	i++;
+}
+return (i);
 }
