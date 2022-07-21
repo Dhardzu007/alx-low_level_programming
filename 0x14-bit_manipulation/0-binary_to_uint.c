e72e0f8f1cc66c49ba46daeed42ebe0f583ffd7c
Rashmi Dayanand <dayanandrashmi2@gmail.com>
2022-07-21 14:43:09 -0700

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -7,18 +7,16 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int num = 0;
-	int len = 0;
+	unsigned int n = 0;
 
-	if (b[len] == '\0')
+	if (!b)
 		return (0);
-
-	while ((b[len] == '0') || (b[len] == '1'))
+	while (*b)
 	{
-		num <<= 1;
-		num += b[len] - '0';
-		len++;
+		if (*b != '0' && *b != '1')
+			return (0);
+		n = n * 2 + *b - '0';
+		++b;
 	}
-
-	return (num);
+	return (n);
 }
