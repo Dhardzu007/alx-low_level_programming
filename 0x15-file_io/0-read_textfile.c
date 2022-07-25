7e42b9c0f0f04329d5511a7520d0e1f2b9fcfdd4
Rashmi Dayanand <dayanandrashmi2@gmail.com>
2022-07-25 10:53:20 -0700

    0-read_textfile.c

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,48 @@
+#include "main.h"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
+/**
+ * read_textfile - reads a text file and prints it to stdout
+ * @filename: path of a filename
+ * @letters: number to letters to read and write
+ * Return: 0 if 'filename is NULL or cannot be open, read or write
+ * or number of bytes to call write
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *buf;
+	int fd;
+	ssize_t r, w;
+
+	if (filename == NULL)
+		return (0);
+	buf = malloc(sizeof(char) * letters);
+	if (buf == NULL)
+		return (0);
+	fd = open(filename, 0_RDWR);
+	if (fd == -1)
+	{
+		free(buf);
+		return (0);
+	}
+	r = read(fd, buf, letters);
+	if (r == -1)
+	{
+		free(buf);
+		close(fd);
+		return (0);
+	}
+	close(fd);
+	w = write(STDOUT_FILENO, buf, r);
+	if (w == -1)
+	{
+		free(buf);
+		return (0);
+	}
+	if (w != r)
+		return (0);
+	return (r);
+}
